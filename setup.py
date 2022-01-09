d71933aa52720a943d3aa62eca92a4eafabd8301
Nick Strange <nsstrange@icloud.com>
2022-01-09 17:22:47 -0500

    added requirements.py and setup.py. removed x.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django_project',
+    version='1.0',
+    packages=['blog', 'blog.migrations', 'users', 'users.migrations', 'django_project'],
+    url='',
+    license='',
+    author='nickstrange',
+    author_email='nsstrange@icloud.com',
+    description=''
+)
--- x.py
+++
@@ -1,9 +0,0 @@
-import json
-x = {'r1': [{'b':'1'}, {'c':2}],
-     'r2': [{'b':'.1'}, {'c':.2}]}
-
-
-print({key: [list(d.values())[0] for d in v1] for key, v1 in x.items()})
-  #  print('lll',(key, value) in [d.items() for d in v1])
-  #  z = [(k, v) for k,v in d.items() for d in value]
- #   print(z)
