6ca96cd24266988f2489486c626b4cc31b969a31
Nick Strange <nsstrange@icloud.com>
2022-01-23 22:06:41 -0500

    combined contacts and works tidyup 2

--- contacts/models.py
+++ contacts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-sort_field='first_name'
+sort_field = 'first_name'
+
 
 # Create your models here.
 class Contact(models.Model):
     title = models.CharField(max_length=6, null=True, blank=True)
-    first_name = models.CharField(max_length=24,null=True, blank=True)
+    first_name = models.CharField(max_length=24, null=True, blank=True)
     last_name = models.CharField(max_length=24, null=True, blank=True)
     phone_number = models.CharField(max_length=24, null=True, blank=True)
     email_address = models.CharField(max_length=24, null=True, blank=True)
@@ -15,8 +16,6 @@ class Contact(models.Model):
     country = models.CharField(max_length=24, null=True, blank=True)
     state = models.CharField(max_length=24, null=True, blank=True)
     post_code = models.CharField(max_length=24, null=True, blank=True)
-
-
 
     def __str__(self):
         return f"{self.title}. {self.first_name} {self.last_name}"
